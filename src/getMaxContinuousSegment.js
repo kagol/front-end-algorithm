c80065990bcdd6ae68896f59321a75dc3db3aa1a
kagol <kagol@sina.com>
2019-01-18 23:13:01 +0800

    optimize code

--- src/getMaxContinuousSegment.js
+++ src/getMaxContinuousSegment.js
@@ -1,23 +1,20 @@
 // 在一个无序整数数组中，找出连续增长片段最长的一段, 增长步长是1。Example: [3,2,4,5,6,1,9], 最长的是[4,5,6]
 function getMaxContinuousSegment(array, step=1){
     var targetMap = new Map();
-    var numberOfRepeats = 1;
+    var numberOfContinuous = 1;
     for(var i=0;i<array.length;i++){
-        var prevItem = array[i];
+        var currentItem = array[i];
         var nextItem = array[i+1];
-        if(nextItem - prevItem === step){
-            numberOfRepeats++;
-            if(numberOfRepeats === 2){
-                targetMap.set(numberOfRepeats, [prevItem, nextItem]);
-            }else if(numberOfRepeats > 2){
-                targetMap.set(numberOfRepeats, targetMap.get(numberOfRepeats-1).concat(nextItem))
-            }
+        if(nextItem - currentItem === step){
+            numberOfContinuous++;
+            var oldItems = numberOfContinuous === 2 ? [currentItem] : targetMap.get(numberOfContinuous-1);
+            targetMap.set(numberOfContinuous, oldItems.concat(nextItem));
         }else{
-            numberOfRepeats = 1;
+            numberOfContinuous = 1;
         }
     }
-    var repeatNumbers = [...targetMap.keys()];
-    var maxKey = Math.max(...repeatNumbers)
+    var continuousNumberArray = [...targetMap.keys()];
+    var maxKey = Math.max(...continuousNumberArray)
     var maxSegment = targetMap.get(maxKey);
     return maxSegment;
 }
