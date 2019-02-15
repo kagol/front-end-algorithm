90b97461a857f3d3599f774d98de5a0cb244fe1f
kagol <kagol@sina.com>
2019-02-15 23:15:56 +0800

    optimize code, a better way to save memory space

--- src/getMaxContinuousSegment.js
+++ src/getMaxContinuousSegment.js
@@ -1,21 +1,22 @@
 // 在一个无序整数数组中，找出连续增长片段最长的一段, 增长步长是1。Example: [3,2,4,5,6,1,9], 最长的是[4,5,6]
 function getMaxContinuousSegment(array, step=1){
-    var targetMap = new Map();
+    var maxLength = 1;
+    var maxIndex = 0;
     var numberOfContinuous = 1;
     for(var i=0;i<array.length;i++){
         var currentItem = array[i];
         var nextItem = array[i+1];
         if(nextItem - currentItem === step){
             numberOfContinuous++;
-            var oldItems = numberOfContinuous === 2 ? [currentItem] : targetMap.get(numberOfContinuous-1);
-            targetMap.set(numberOfContinuous, oldItems.concat(nextItem));
         }else{
+            if(numberOfContinuous > maxLength){
+                maxLength = numberOfContinuous;
+                maxIndex = i - numberOfContinuous + 1;
+            }
             numberOfContinuous = 1;
         }
     }
-    var continuousNumberArray = [...targetMap.keys()];
-    var maxKey = Math.max(...continuousNumberArray)
-    var maxSegment = targetMap.get(maxKey);
+    var maxSegment = array.slice(maxIndex, maxIndex+maxLength);
     return maxSegment;
 }
 
