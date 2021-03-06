ef950b8f7b8709f1fca27e446a1be04c202487b4
kagol <kagol@sina.com>
2021-03-06 13:05:04 +0800

    feat: add sharedLetters

--- src/sharedLetters.js
+++ src/sharedLetters.js
@@ -2,13 +2,45 @@
  * 多个单词组成的数组，找出所有单词都有的字符（可重复，顺序无关）
  * 返回这些重复字符组成的字符串
  */
-const sharedLetters = (letterArr) => {
-
+const sharedLetters = (wordArr) => {
+  let targetLetters = ''
+  const baseWord = wordArr[0]
+  let otherWords = wordArr.filter(item => item !== baseWord)
+  baseWord.split('').forEach(baseLetter => {
+    if (allExist(otherWords, baseLetter)) {
+      targetLetters += baseLetter
+      otherWords = allExclude(otherWords, baseLetter)
+    }
+  })
+  return targetLetters
 }
 
-const letterArr = ['smooth', 'common', 'mooring']
+const allExist = (arr, letter) => {
+  return arr.every(item => item.includes(letter))
+}
+
+const exclude = (word, letter) => {
+  let result = ''
+  let first = true
+  word.split('').forEach(item => {
+    if (item === letter && first) {
+      first = false
+    } else {
+      result += item
+    }
+  })
+  return result
+}
+
+const allExclude = (arr, letter) => {
+  return arr.map(item => {
+    return exclude(item, letter)
+  })
+}
+
+const wordArr = ['smooth', 'common', 'mooring']
 /**
  * 三个单词都有'm'，并且都有两个'o'，返回'moo'（'omo', 'oom'也可）
  */
-const result = sharedLetters(letterArr)
+const result = sharedLetters(wordArr)
 console.log('result:', result)
